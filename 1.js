f167e36a998c6b7634612f63827528a910bef39c
harsh <rohillah697@gmail.com>
2026-07-21 19:21:38 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,50 @@
+// console.log("welcome");
+// console.log("my name is harsh");
+
+
+// name="harsh";
+// age=25;
+// price=69;
+// x=null;
+// y=undefined;
+// isfollow=true;
+// console.log(isfollow);
+
+// fullname="rrrrharsh";
+// _fullname=" roharsh";
+// $fullname="rrharsh";
+
+// console.log(_fullname);
+// const PI=3.14;
+// console.log(PI);
+
+// { 
+//     let a=5;
+//     console.log(a);
+// }
+// {
+//     let a=8;
+//     console.log(a);
+// }
+
+
+// const student = {
+//     name :"harsh",
+//     age: 45,
+//     cgpa :7.7,
+//     ispass:true,
+// };
+// student ["name"]="rahul";
+
+// console.log(student["name"]);
+ 
+
+
+// const product ={
+//     name : "ball pen",
+//     rating :4,
+//     offer: 5,
+//     price:560,
+// };
+
+// console.log(product);

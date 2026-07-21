bb5588b8f9525fa8b58ea1ef5218449c8b7a2784
harsh <rohillah697@gmail.com>
2026-07-21 21:34:04 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,89 @@
+// let a=5;
+// let b=3;
+
+// console.log("a+b=", a + b);
+// console.log("a-b=", a -  b);
+// console.log("a*b=", a *  b);
+// console.log("a**b=", a **  b);
+
+
+// let a=5;
+// let b=3;
+
+// console.log("a=",a,"b=",b,);
+// a++;
+// b--;
+// console.log("a=",a,"b=",b);
+
+
+
+// let a=5;
+// let b=3;
+// console.log("a=",a,"b=",b,);
+// ++a;
+// console.log("++a=",a,);
+//  console.log("a=",a,);
+
+
+
+// let a = 3;
+// let b = 4;
+// a+=4;
+// console.log("a=", a);
+
+
+// let age =25;
+
+
+// if(age>18) {
+//     console.log("you can vote");
+// }
+
+
+// if(age<18){
+//     console.log("you cant");
+// // }
+ 
+
+//  let name=prompt("hello");
+//  console.log(name);
+
+ 
+
+
+// let num = prompt("enter a number");
+
+// if (num % 5 === 0){
+//     console.log(num, "is a multiple");
+// }else{
+//     console.log(num, "not a multiple");
+// }
+
+
+let score = 4;
+let grade;
+
+if (score => 90 && score <= 100) {
+    grade = "A";
+} else if (score => 70 && score <= 89) {
+  grade = "B";
+} else if (score => 60 && score <= 69) {
+  grade = "C";
+} else if(score => 50 && score <= 59){
+  grade = "D";
+} else if(score =>0 && score <= 49){
+  grade = "F";
+}
+
+console.log(grade);
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3.js
@@ -0,0 +1,59 @@
+// for (let count=1;count<=5; count++){
+//     console.log("harsh")
+// }
+
+
+// let sum=0;
+//  for(let i=1; i<=5; i++){
+//     sum = sum +i;
+//     console.log("sum=",sum);
+//  }
+
+
+// let i =20;
+// do{
+//     console.log("harsh");
+//     i++
+// }while(i<=10)
+
+  
+
+// for (let i=0;i<=100;i++){
+//     if(i%2 != 0)
+//         console.log("even num=",i);
+// }
+
+
+
+// let gamenum=25;
+// let usernum = prompt("guess the num game:");
+
+// while(usernum != gamenum){
+//   usernum=prompt("guess the no. AGAIN:");
+// }
+// console.log("cong!!!!");
+
+
+// let str = "harsh rohilla";
+// console.log(str[4]);
+
+
+
+// let obj={
+//     price: 345,
+//     item:"pen",
+// };
+
+// let output =`the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+
+// \n "sentence starts from next line "
+// \t " space bt words "
+
+
+
+
+// let fullname=prompt("enetr your name");
+// let username= "@" + fullname+ fullname.length;
+// console.log(username);

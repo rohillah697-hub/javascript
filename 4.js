9864ffcd59f718cdae3a974b22a0c360dbc5f598
harsh <rohillah697@gmail.com>
2026-07-23 13:46:47 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,42 @@
+// let hero=["harsh","anh","anush","jajh"];
+// // // console.log(hero);
+
+
+// // let array=[78,88,99,0,7,6];
+// // array[0]  = 100;
+// // console.log(array);
+
+
+
+// for(let idx=0; idx < hero.length; idx++){
+//     console.log(hero[idx])
+// }
+
+
+
+// let st=["patoto","mango","tamato","apple"];
+// // console.log(st.toString());
+// console.log(st.splice(0,1,));
+
+
+
+// let arr = [1,2,3,4,5,6,7,];
+// arr.splice(2,0,10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

62dd5f082117cc86aae43e7073fb3563defd6a9e
harsh <rohillah697@gmail.com>
2026-08-05 09:47:26 +0530

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,88 @@
+// function myfunction(){
+//     console.log("my name is harsh");
+//     console.log("mmhhh");
+// }
+// myfunction();
+
+
+
+// function sum(a,b){
+//     console.log(a+b);
+
+// }
+
+
+// morden js
+//  const arrowSum = (a,b) => {
+//     // console.log(a+b);
+//     return a * b;
+// };
+
+
+
+
+// function countVowels(str){
+
+// }
+
+// function countVowels(str) {
+//     let count = 0;
+//     for (const char of str) {
+//         if (
+//             char ==="a" ||
+//             char ==="e" ||
+//             char ==="i" ||
+//             char ==="o" ||
+//             char ==="u" ||
+//         ){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+
+// let num =[1,2,3,4,];
+
+// // arr.forEach(function printVal(val){
+// //     console.log(val);
+// // });
+
+// // num.forEach((num) =>{
+// //     console.log(num*num);
+// // });
+
+//  let evenar=num.filter((val)=>{
+//     return val % 2 === 0;
+// });
+// console.log(evenar);
+
+
+
+// let arr =[8,98,4,3,1];
+
+// const output = arr.reduce((prev,curr) =>{
+//     return prev > curr ? prev:curr;
+// });
+// console.log(output);
+
+
+
+// let arr =[8,98,4,3,1];
+
+// const output = arr.reduce((prev,curr) =>{
+//     return prev < curr ? prev:curr;
+// });
+
+// console.log(output);
+
+
+
+// let arr =[89,78,65,73,82,91,98,97,];
+ 
+//  let marks=arr.filter((val) =>{
+//     return val>90
+// // });
+// // console.log(marks);
+
+
---
+++ 6.js
@@ -0,0 +1,17 @@
+// // let h1 =document.querySelector("h1");
+// // console.log("h1");
+
+
+// // h1.innerText=h1.innerText+" my anme is ";
+
+
+// let divs =document.querySelectorAll("div");
+// // console.log(divs[0]);
+// divs[1].innerText="new value";
+
+
+
+
+
+
+

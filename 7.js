50d25aa360fc4e560e132a78f74edfc428608ea2
harsh <rohillah697@gmail.com>
2026-08-06 11:43:54 +0530

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,39 @@
+// let div =document.querySelector("div");
+// console.log(div);
+
+
+// let id=div.getAttribute("id");
+// console.log("id");
+
+
+
+// let div =document.querySelector("div");
+// console.log(all.getAttribute("id"));
+
+
+
+
+// let div =document.querySelector("div");
+// console.log(all.setAttribute("id"," new idd"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 8.js
@@ -0,0 +1,24 @@
+// let newBtn = document.createElement("button");
+// newBtn.innerText= "click me";
+// console.log(newBtn);
+
+// const { createElement } = require("react");
+
+
+// let div = document.querySelector("div");
+// div.append(newBtn);
+// // div.remove();
+
+
+
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText="click me";
+// newBtn.style.color ="white";
+// newBtn.style.backgroundColor ="red";
+
+// document.querySelector("body").prepend(newBtn);
+
+
+
+
